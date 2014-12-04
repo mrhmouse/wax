cbaa2a76b7bbcaddba929a7bf326d8c02ecd975d
Jordan Brown <mrhmouse@gmail.com>
2014-12-04 16:21:24 -0500

    Cleanup InvertVisitor
    Use the methods provided by the parent
    class instead of a conditional ladder

--- InvertVisitor.cs
+++ InvertVisitor.cs
@@ -1,5 +1,6 @@
 ﻿namespace ExpressionKit.Unwrap
 {
+  using System;
   using System.Linq.Expressions;
   using System.Reflection;
 
@@ -14,88 +15,88 @@
       return Expression.Equal(e, Expression.Constant(false));
     }
 
-    public override Expression Visit(Expression e)
+    protected override Expression VisitBinary(BinaryExpression b)
     {
-      if (e is BinaryExpression)
+      switch (b.NodeType)
       {
-        var b = e as BinaryExpression;
-        switch (b.NodeType)
-        {
-          case ExpressionType.AndAlso:
-            return Expression.OrElse(
-              new InvertVisitor().Visit(b.Left),
-              new InvertVisitor().Visit(b.Right));
+        case ExpressionType.AndAlso:
+          return Expression.OrElse(
+            new InvertVisitor().Visit(b.Left),
+            new InvertVisitor().Visit(b.Right));
 
-          case ExpressionType.OrElse:
-            return Expression.AndAlso(
-              new InvertVisitor().Visit(b.Left),
-              new InvertVisitor().Visit(b.Right));
+        case ExpressionType.OrElse:
+          return Expression.AndAlso(
+            new InvertVisitor().Visit(b.Left),
+            new InvertVisitor().Visit(b.Right));
 
-          case ExpressionType.Equal:
-            return Expression.NotEqual(
-              b.Left,
-              b.Right);
+        case ExpressionType.Equal:
+          return Expression.NotEqual(
+            b.Left,
+            b.Right);
 
-          case ExpressionType.NotEqual:
-            return Expression.Equal(
-              b.Left,
-              b.Right);
+        case ExpressionType.NotEqual:
+          return Expression.Equal(
+            b.Left,
+            b.Right);
 
-          case ExpressionType.GreaterThanOrEqual:
-            return Expression.LessThan(
-              b.Left,
-              b.Right);
+        case ExpressionType.GreaterThanOrEqual:
+          return Expression.LessThan(
+            b.Left,
+            b.Right);
 
-          case ExpressionType.GreaterThan:
-            return Expression.LessThanOrEqual(
-              b.Left,
-              b.Right);
+        case ExpressionType.GreaterThan:
+          return Expression.LessThanOrEqual(
+            b.Left,
+            b.Right);
 
-          case ExpressionType.LessThanOrEqual:
-            return Expression.GreaterThan(
-              b.Left,
-              b.Right);
+        case ExpressionType.LessThanOrEqual:
+          return Expression.GreaterThan(
+            b.Left,
+            b.Right);
 
-          case ExpressionType.LessThan:
-            return Expression.GreaterThanOrEqual(
-              b.Left,
-              b.Right);
-        }
+        case ExpressionType.LessThan:
+          return Expression.GreaterThanOrEqual(
+            b.Left,
+            b.Right);
+
+        default:
+          return b;
       }
-      else if (e is UnaryExpression)
+    }
+
+    protected override Expression VisitUnary(UnaryExpression u)
+    {
+      switch (u.NodeType)
       {
-        var u = e as UnaryExpression;
-        switch (u.NodeType)
-        {
-          case ExpressionType.IsTrue:
-            return Invert(u.Operand);
+        case ExpressionType.IsTrue:
+          return Invert(u.Operand);
 
-          case ExpressionType.IsFalse:
-          case ExpressionType.Not:
-            return u.Operand;
-        }
+        case ExpressionType.IsFalse:
+        case ExpressionType.Not:
+          return u.Operand;
+
+        default:
+          return u;
       }
-      else if (e is MemberExpression)
+    }
+
+    protected override Expression VisitMember(MemberExpression m)
+    {
+      Type t;
+      if (m.Member is PropertyInfo)
       {
-        var m = e as MemberExpression;
-        var transform = false;
-        var boolean = typeof(bool);
-
-        if (m.Member is PropertyInfo)
-        {
-          var p = m.Member as PropertyInfo;
-          transform = boolean == p.PropertyType;
-        }
-        else if (m.Member is FieldInfo)
-        {
-          var f = m.Member as FieldInfo;
-          transform = boolean == f.FieldType;
-        }
-
-        if (transform) return Invert(e);
+        var p = m.Member as PropertyInfo;
+        t = p.PropertyType;
       }
+      else if (m.Member is FieldInfo)
+      {
+        var f = m.Member as FieldInfo;
+        t = f.FieldType;
+      }
+      else return m;
 
-      return e;
+      if (t == typeof(bool)) return Invert(m);
+      else return m;
     }
   }
 }
